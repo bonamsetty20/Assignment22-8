d91b4e17edb96522b5c8fb0d44459cdcd64fbd37
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-08-03 15:20:05 +0530

    first commit

---
+++ Destination.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment21
+{
+    public class Destination
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Salary { get; set; }
+        public string Product { get; internal set; }
+        public double Price { get; internal set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Source source = new Source()
+            {
+                Id = 1,
+                Name = "Oneplus",
+                Product = "CPH"
+
+            };
+
+            Destination destination = new Destination()
+            {
+                Id = 2,
+                Name = "",
+                Product = "",
+                Price = 45000
+            };
+
+            Console.WriteLine("Source");
+            Console.WriteLine($"ID: {source.Id} Name: {source.Name} Product: {source.Product}");
+
+            Console.WriteLine("\nDestination");
+            Console.WriteLine($"ID: {destination.Id} Name: {destination.Name} Product: {destination.Product} Price: {destination.Price}");
+
+            DynamicPropertyMapping mapping = new DynamicPropertyMapping();
+            mapping.MapProperties(source, destination);
+            Console.WriteLine("\nDestination After mapping");
+            Console.WriteLine($"ID: {destination.Id} Name: {destination.Name} Product: {destination.Product} Price: {destination.Price}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
